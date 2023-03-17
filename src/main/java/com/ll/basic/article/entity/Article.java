9153873cc6b71ab9eee995836a3a0d903d0a9576
jny0 <pjy100402@naver.com>
2023-03-17 12:23:34 +0900

    Article 클래스에 생성자 추가

--- src/main/java/com/ll/basic/article/entity/Article.java
+++ src/main/java/com/ll/basic/article/entity/Article.java
@@ -3,8 +3,10 @@ package com.ll.basic.article.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +15,8 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Entity
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Article {
     @Id // PRIMARY KEY
     @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
