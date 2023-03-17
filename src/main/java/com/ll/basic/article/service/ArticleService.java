78560945bb7c9e4ccb278ea07326bf3e336a7b55
jny0 <pjy100402@naver.com>
2023-03-17 15:36:14 +0900

    등록날짜, 수정날짜 자동갱신

--- src/main/java/com/ll/basic/article/service/ArticleService.java
+++ src/main/java/com/ll/basic/article/service/ArticleService.java
@@ -2,13 +2,8 @@ package com.ll.basic.article.service;
 
 import com.ll.basic.article.entity.Article;
 import com.ll.basic.article.repository.ArticleRepository;
-import com.ll.basic.base.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -18,8 +13,6 @@ public class ArticleService {
     public Article write(String title, String body){
         Article article = Article
                 .builder()
-                .createDate(LocalDateTime.now())
-                .modifyDate(LocalDateTime.now())
                 .title(title)
                 .body(body)
                 .build();
